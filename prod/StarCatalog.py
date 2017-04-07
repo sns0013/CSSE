fd08285715c6ccfae4155697bfdaf338aaf11930
Sydney <sns0013@auburn.edu>
2017-04-07 17:49:09 -0500

    Green Light |

--- prod/StarCatalog.py
+++ prod/StarCatalog.py
@@ -60,7 +60,8 @@ StarCatalog = {
     'markab':'13d36.7,15d17.6'}
 
 def getStar(starName):
-    if starName.lower() in StarCatalog:
+    starName = starName.lower()
+    if starName in StarCatalog:
         return StarCatalog[starName]
     else:
         return 'error'
