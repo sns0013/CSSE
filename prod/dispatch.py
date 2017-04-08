a4c4fff34d8fe1696d52b4cfb5202982f2efafbe
Sydney <sns0013@auburn.edu>
2017-04-07 20:11:06 -0500

    Refactor |

--- prod/dispatch.py
+++ prod/dispatch.py
@@ -233,7 +233,8 @@ def predict(values):
     AriesGHA = total(PM, obsRot)
     GHA = calculateGHA(AriesGHA, sideRealAngle)
     splitGHA = str(GHA).split('.')
-    minutes = ((GHA - int(splitGHA[0])) * 60) - (((GHA - int(splitGHA[0])) * 60) % 0.01)
+    minutes = ((GHA - int(splitGHA[0])) * 60)
+    minutes = minutes * 100 / 100
     formatLongitude = '%d'%(int(splitGHA[0]) % 360) + 'd' + '%.1f'%(minutes)
     values['longitude'] = formatLongitude
     return values
