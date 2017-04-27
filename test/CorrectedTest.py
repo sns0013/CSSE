b2d6e327f65f99f381bcf4c04c255d9b7e5997ed
Sydney <sns0013@auburn.edu>
2017-04-27 13:48:35 -0500

    Red Light |

--- test/CorrectedTest.py
+++ test/CorrectedTest.py
@@ -5,184 +5,184 @@ import prod.StarCatalog as SC
 
 class CorrectedTest(unittest.TestCase):
     def test001_000_lat_LBX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_lat_HBX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_lat_NX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_lat_EX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_lat_LBY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_lat_HBY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_lat_NY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_lat_EY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_lat_Missing(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_lat_IncorrectFormat(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
 
     def test001_000_long_LBX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_long_HBX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_long_NX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_long_EX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_long_LBY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_long_HBY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_long_NY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_long_EY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_long_Missing(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_long_IncorrectFormat(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
 
     def test001_000_alt_LBX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_alt_HBX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_alt_NX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_alt_EX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_alt_LBY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_alt_HBY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_alt_NY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_alt_EY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_alt_Missing(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_alt_IncorrectFormat(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
 
     def test001_000_assLat_LBX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLat_HBX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLat_NX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLat_EX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLat_LBY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLat_HBY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLat_NY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLat_EY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLat_Missing(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLat_IncorrectFormat(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
 
     def test001_000_assLong_LBX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLong_HBX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLong_NX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLong_EX(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLong_LBY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLong_HBY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLong_NY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLong_EY(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLong_Missing(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_assLong_IncorrectFormat(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
 
     def test001_000_correctedDistance_Present(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_correctedAzi_Present(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_correctedDistance_Correct(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_correctedAzi_Correct(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
 
     def test001_000_calculateLHA_Correct(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_calculateCorrectAlt_Correct(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_calculateCorrectDist_Correct(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
     def test001_000_calculateCorrectAzi_Correct(self):
-        self.assertTrue(self, 1, 1);
+        self.assertTrue(True);
 
 
 
